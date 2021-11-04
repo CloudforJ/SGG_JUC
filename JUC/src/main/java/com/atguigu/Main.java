43702a83fc0da9520613d9ac02640c1629d09bd7
CloudforJ <bowenlu.cloud@gmail.com>
2021-11-04 13:52:37 +0800

    Create Main.java

---
+++ JUC/src/main/java/com/atguigu/Main.java
@@ -0,0 +1,15 @@
+package com.atguigu;
+
+public class Main {
+    public static void main(String[] args) {
+        Thread aa = new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + "::" + Thread.currentThread().isDaemon());
+            while (true) {
+
+            }
+        }, "aa");
+        aa.start();
+
+        System.out.println(Thread.currentThread().getName() + " over");
+    }
+}
